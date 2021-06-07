bcdb08df93a5efd3b5525dd6bcf1cf0228c11f4c
Jesus Montano <jesus.montano@tradeshift.com>
2021-06-07 16:19:04 -0700

    [JM-3] Add Twilio integration
    Add Twilio in order to be able to send text alerts.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 import { Retailer } from './types'
+require('dotenv').config()
+const twilio = require('twilio')
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+const client = new twilio(accountSid, authToken)
+
+client.messages
+    .create({
+        body: 'Hello! This is PS5 Bot.',
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.PERSONAL_PHONE_NUMBER,
+    })
+    .then((message) => console.log(message))
 
 const AxiosInstance = axios.create()
 
